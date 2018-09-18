8be9cdc9b09f0e11345d85b9f7daf116afbcc725
Matt Duffield <matt.duffield@gmail.com>
2018-09-17 21:30:06 -0700

    Auto commit from FrontEnd Creator

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia Babel SystemJS';
     config.map([
-      {"route": "welcome", "name": "welcome", "moduleId": PLATFORM.moduleName("./welcome"), "nav": true, "title": "Welcome"},
+      {"route": ["", "welcome"], "name": "welcome", "moduleId": PLATFORM.moduleName("./welcome"), "nav": true, "title": "Welcome"},
       {"route": "users", "name": "users", "moduleId": PLATFORM.moduleName("./users"), "nav": true, "title": "GitHub Users"},
       {"route": "child-router", "name": "child-router", "moduleId": PLATFORM.moduleName("./child-router"), "nav": true, "title": "Child Router"},
-      {"route": ["", "generator-view"], "name": "generator-view", "moduleId": PLATFORM.moduleName("./views/generator-view"), "nav": true, "title": "Generators"}
+      {"route": "generator-view", "name": "generator-view", "moduleId": PLATFORM.moduleName("./views/generator-view"), "nav": true, "title": "Generators"}
     ]);
 
     this.router = router;
