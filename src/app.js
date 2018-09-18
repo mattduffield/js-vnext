4d3b2c4aa632a5690ac31d54f5c72b444428a138
Matt Duffield <matt.duffield@gmail.com>
2018-09-17 21:12:30 -0700

    Auto commit from FrontEnd Creator

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ export class App {
       {"route": ["", "welcome"], "name": "welcome", "moduleId": PLATFORM.moduleName("./welcome"), "nav": true, "title": "Welcome"},
       {"route": "users", "name": "users", "moduleId": PLATFORM.moduleName("./users"), "nav": true, "title": "GitHub Users"},
       {"route": "child-router", "name": "child-router", "moduleId": PLATFORM.moduleName("./child-router"), "nav": true, "title": "Child Router"},
-      {"route": "generator-view", "name": "generator-view", "moduleId": PLATFORM.moduleName("./views/generator-view/generator-view"), "nav": true, "title": "Generators"}
+      {"route": "generator-view", "name": "generator-view", "moduleId": PLATFORM.moduleName("./views/generator-view"), "nav": true, "title": "Generators"}
     ]);
 
     this.router = router;
